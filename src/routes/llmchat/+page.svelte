817ed23be43cd96112f26a8388646e234f052aed
dagmawibabi <1babidagi@gmail.com>
2026-03-05 21:13:49 +0300

    refactored

--- src/routes/llmchat/+page.svelte
+++ src/routes/llmchat/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-	import AiChat from '../../sections/ai_chat.svelte';
+	import AiChat from '../../components/portfolio_components/sections/ai_chat.svelte';
 </script>
 
 <svelte:head>
 	<title>Chat | Dagmawi Babi</title>
 </svelte:head>
 
-<div class="no-scrollbar mx-auto w-[96%] pb-56 pt-4 md:w-[96%] lg:w-1/2 xl:w-1/2 2xl:w-1/2">
+<div class="no-scrollbar mx-auto w-[96%] pt-4 pb-56 md:w-[96%] lg:w-1/2 xl:w-1/2 2xl:w-1/2">
 	<AiChat isFullscreen={true} />
 </div>
